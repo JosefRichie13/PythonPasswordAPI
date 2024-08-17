7a2d7d70ff2599f39181df405c4e94de3c7a9c16
Joseph Richie <joseph.richie@qumu.com>
2024-08-17 09:45:00 +0530

    Added CREATE and READ endpoints

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 import shortuuid
 import time
 import hashlib
+import cryptocode
+
 
 
 # Generates a UUID, which is used as an ID for all comments and replies
@@ -18,3 +20,23 @@ def hashTheAdminPasswordUsingSHA(userid, password, timstamp):
     saltedPassword = userid + password + str(timstamp)
     hashedPassword = hashlib.sha256(saltedPassword.encode())
     return hashedPassword.hexdigest()
+
+# Encrypts a UserID and returns it
+def encryptAUserID(ID, userid, timestamp):
+    keyToEncrypt = ID + str(timestamp)
+    return cryptocode.encrypt(userid, keyToEncrypt)
+
+# Decrypts a UserID and returns it
+def decryptAUserID(ID, userid, timestamp):
+    keyToDecrypt = ID + str(timestamp)
+    return cryptocode.decrypt(userid, keyToDecrypt)
+
+# Encrypts a Password and returns it
+def encryptAPassword(ID, password, timestamp):
+    keyToEncrypt = str(timestamp) + ID
+    return cryptocode.encrypt(password, keyToEncrypt)
+
+# Decrypts a Password and returns it
+def decryptAPassword(ID, password, timestamp):
+    keyToDecrypt = str(timestamp) + ID
+    return cryptocode.decrypt(password, keyToDecrypt)
--- index.py
+++ index.py
@@ -37,7 +37,7 @@ def updatePasswordAdmin(updatePasswordAdminBody: updatePasswordAdminData, respon
     queryToCheckAdminPassword = "SELECT PASSWORD FROM PASSWORDDB WHERE USERID = 'PASSWORDADMIN' AND DOMAIN = 'PASSWORDADMIN' AND ID = 'PASSWORDADMIN'"
     adminPasswordCheck = cur.execute(queryToCheckAdminPassword).fetchone()
 
-    # Checks if the current password's hash is the same as the one stores
+    # Checks if the current password's hash is the same as the one stored
     if currentPasswordAdminPassword(updatePasswordAdminBody.oldPassword) == adminPasswordCheck[0]:
 
         # If yes, gets the current epoch time and stores it
@@ -56,3 +56,158 @@ def updatePasswordAdmin(updatePasswordAdminBody: updatePasswordAdminData, respon
     # If the current password is not the same, reject with a 403
     response.status_code = status.HTTP_403_FORBIDDEN
     return {"status" : "Your password is incorrect"}
+
+
+
+# Body for POST AND PUT
+class addLoginData(BaseModel):
+    adminPassword : str
+    domain : str
+    userID : str
+    password: str
+
+# Body for GET and DELETE 
+class getLoginData(BaseModel):
+    adminPassword : str
+
+
+# Adds a Login detail record
+@app.post("/addLoginDetail")
+def addALoginDetail(addLoginBody: addLoginData, response: Response):
+
+    postConnection = sqlite3.connect("PASSWORDDB.db")
+    cur = postConnection.cursor()
+
+    # Gets the current password stored as a hash from the DB
+    queryToCheckAdminPassword = "SELECT PASSWORD FROM PASSWORDDB WHERE USERID = 'PASSWORDADMIN' AND DOMAIN = 'PASSWORDADMIN' AND ID = 'PASSWORDADMIN'"
+    adminPasswordCheck = cur.execute(queryToCheckAdminPassword).fetchone()
+
+    # Checks if the current password's hash is the same as the one stored
+    if currentPasswordAdminPassword(addLoginBody.adminPassword) == adminPasswordCheck[0]:
+
+        # If the Admin password is correct, it checks if the supplied domain already exists in the DB 
+        queryToCheckExistingDomain = "SELECT DOMAIN FROM PASSWORDDB WHERE DOMAIN = ?"
+        valueToCheckExistingDomain = [addLoginBody.domain]
+        existingDomainCheck = cur.execute(queryToCheckExistingDomain, valueToCheckExistingDomain).fetchone()
+        
+        # If the supplied domain is not in the DB, add the login details to the DB 
+        if existingDomainCheck is None:
+
+            generatedIDForLogin = generateID()
+            generatedTimestamp = currentEpochTime()
+            encryptedUserID = encryptAUserID(generatedIDForLogin, addLoginBody.userID, generatedTimestamp)
+            encryptedPassword = encryptAPassword(generatedIDForLogin, addLoginBody.password, generatedTimestamp)
+
+            queryToAddALogin = "INSERT INTO PASSWORDDB (ID, DOMAIN, USERID, PASSWORD, TIMESTAMP) Values (?, ?, ?, ?, ?)"
+            valuesToAddALogin = (generatedIDForLogin, addLoginBody.domain, encryptedUserID, encryptedPassword, generatedTimestamp)
+            cur.execute(queryToAddALogin, valuesToAddALogin)
+            postConnection.commit()
+
+            return {"status" : "Login added for " + addLoginBody.domain}
+
+    # If the Admin password is wrong or the supplied domain already exists in the DB, reject with 403 
+    response.status_code = status.HTTP_403_FORBIDDEN
+    return {"status" : "Your admin password is incorrect or a login already exists for " + addLoginBody.domain + ". Please recheck"}
+
+
+
+# Retrieves the password by the Domain
+@app.get("/getPassword")
+def getPasswordByDomain(domain: str, getLoginBody: getLoginData, response: Response):
+
+    getConnection = sqlite3.connect("PASSWORDDB.db")
+    cur = getConnection.cursor()
+
+    # Gets the current password stored as a hash from the DB
+    queryToCheckAdminPassword = "SELECT PASSWORD FROM PASSWORDDB WHERE USERID = 'PASSWORDADMIN' AND DOMAIN = 'PASSWORDADMIN' AND ID = 'PASSWORDADMIN'"
+    adminPasswordCheck = cur.execute(queryToCheckAdminPassword).fetchone()
+
+    # Checks if the current password's hash is the same as the one stored
+    if currentPasswordAdminPassword(getLoginBody.adminPassword) == adminPasswordCheck[0]:
+
+        # If the Admin password is correct, it checks if the supplied domain exists in the DB 
+        queryToCheckExistingDomain = "SELECT * FROM PASSWORDDB WHERE DOMAIN = ?"
+        valueToCheckExistingDomain = [domain]
+        existingDomainCheck = cur.execute(queryToCheckExistingDomain, valueToCheckExistingDomain).fetchone()
+
+        # If the supplied domain exists in the DB, return the password
+        # Else, reject with a 404
+        if existingDomainCheck is not None:
+            decryptedPassword = decryptAPassword(existingDomainCheck[0], existingDomainCheck[3], existingDomainCheck[4])
+            return {"Password" : decryptedPassword, "Domain": domain}
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"status" : "There is no record for " + domain + ". Please recheck"}
+
+    # If the Admin password is wrong reject with 403 
+    response.status_code = status.HTTP_403_FORBIDDEN
+    return {"status" : "Your admin password is incorrect. Please recheck"}
+
+
+
+# Retrieves the UserID by the Domain
+@app.get("/getUserID")
+def getPasswordByDomain(domain: str, getLoginBody: getLoginData, response: Response):
+
+    getConnection = sqlite3.connect("PASSWORDDB.db")
+    cur = getConnection.cursor()
+
+    # Gets the current password stored as a hash from the DB
+    queryToCheckAdminPassword = "SELECT PASSWORD FROM PASSWORDDB WHERE USERID = 'PASSWORDADMIN' AND DOMAIN = 'PASSWORDADMIN' AND ID = 'PASSWORDADMIN'"
+    adminPasswordCheck = cur.execute(queryToCheckAdminPassword).fetchone()
+
+    # Checks if the current password's hash is the same as the one stored
+    if currentPasswordAdminPassword(getLoginBody.adminPassword) == adminPasswordCheck[0]:
+
+        # If the Admin password is correct, it checks if the supplied domain exists in the DB 
+        queryToCheckExistingDomain = "SELECT * FROM PASSWORDDB WHERE DOMAIN = ?"
+        valueToCheckExistingDomain = [domain]
+        existingDomainCheck = cur.execute(queryToCheckExistingDomain, valueToCheckExistingDomain).fetchone()
+
+        # If the supplied domain exists in the DB, return the UserID
+        # Else, reject with a 404
+        if existingDomainCheck is not None:
+            decryptedUserID = decryptAUserID(existingDomainCheck[0], existingDomainCheck[2], existingDomainCheck[4])
+            return {"UserID" : decryptedUserID, "Domain": domain}
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"status" : "There is no record for " + domain + ". Please recheck"}
+
+    # If the Admin password is wrong reject with 403 
+    response.status_code = status.HTTP_403_FORBIDDEN
+    return {"status" : "Your admin password is incorrect. Please recheck"}
+
+
+
+# Retrieves the Crendentials (UserID and Password) by the Domain
+@app.get("/getCredentials")
+def getPasswordByDomain(domain: str, getLoginBody: getLoginData, response: Response):
+
+    getConnection = sqlite3.connect("PASSWORDDB.db")
+    cur = getConnection.cursor()
+
+    # Gets the current password stored as a hash from the DB
+    queryToCheckAdminPassword = "SELECT PASSWORD FROM PASSWORDDB WHERE USERID = 'PASSWORDADMIN' AND DOMAIN = 'PASSWORDADMIN' AND ID = 'PASSWORDADMIN'"
+    adminPasswordCheck = cur.execute(queryToCheckAdminPassword).fetchone()
+
+    # Checks if the current password's hash is the same as the one stored
+    if currentPasswordAdminPassword(getLoginBody.adminPassword) == adminPasswordCheck[0]:
+
+        # If the Admin password is correct, it checks if the supplied domain exists in the DB 
+        queryToCheckExistingDomain = "SELECT * FROM PASSWORDDB WHERE DOMAIN = ?"
+        valueToCheckExistingDomain = [domain]
+        existingDomainCheck = cur.execute(queryToCheckExistingDomain, valueToCheckExistingDomain).fetchone()
+
+        # If the supplied domain exists in the DB, return the UserID and Password
+        # Else, reject with a 404
+        if existingDomainCheck is not None:
+            decryptedUserID = decryptAUserID(existingDomainCheck[0], existingDomainCheck[2], existingDomainCheck[4])
+            decryptedPassword = decryptAPassword(existingDomainCheck[0], existingDomainCheck[3], existingDomainCheck[4])
+            return {"UserID" : decryptedUserID, "Password" : decryptedPassword, "Domain" : domain}
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"status" : "There is no record for " + domain + ". Please recheck"}
+
+    # If the Admin password is wrong reject with 403 
+    response.status_code = status.HTTP_403_FORBIDDEN
+    return {"status" : "Your admin password is incorrect. Please recheck"}
